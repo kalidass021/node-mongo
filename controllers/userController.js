cdf8565b18a55ee909888d5d1323200cb2a18339
Kalidass <kalidass.k021@gmail.com>
2025-04-16 16:26:03 +0530

    update user data controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -49,6 +49,37 @@ export const getSpecificUser = async (req, res, next) => {
   }
 };
 
+export const updateUser = async (req, res, next) => {
+  try {
+    const { id: userId } = req.params;
+
+    if (!userId) {
+      return res
+        .status(400)
+        .json({ message: 'User Id is required for update operation' });
+    }
+
+    const update = req.body;
+    if (!update) {
+      return res
+        .status(400)
+        .json({ message: 'Please provide details that needs to be updated' });
+    }
+    // by default findByIdAndUpdate method returns the document before updation
+    // if we want the document after the updation done, we need pass returnDocument: 'after'
+    const updatedUser = await User.findByIdAndUpdate(userId, update, {
+      returnDocument: 'after',
+    });
+
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    console.error(
+      `Error while updating the user data ${err?.message} ?? ${err}`
+    );
+    next(err);
+  }
+};
+
 export const deleteUser = async (req, res, next) => {
   try {
     const { id: userId } = req.params;
@@ -63,10 +94,10 @@ export const deleteUser = async (req, res, next) => {
     const user = await User.findByIdAndDelete(userId);
 
     if (!user) {
-      return res.status(404).json({message: 'User not found'});
+      return res.status(404).json({ message: 'User not found' });
     }
 
-    res.status(200).json({message: 'User removed', deletedUser: user});
+    res.status(200).json({ message: 'User removed', deletedUser: user });
   } catch (err) {
     console.error(`Error while user deletion: ${err?.message} ?? ${err}`);
     next(err);
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,18 @@
 import { Router } from 'express';
-import { signup, fetchAllUsers, getSpecificUser, deleteUser } from '../controllers/userController.js';
+import {
+  signup,
+  fetchAllUsers,
+  getSpecificUser,
+  deleteUser,
+  updateUser,
+} from '../controllers/userController.js';
 
 const router = Router();
 
 router.post('/signup', signup);
 router.get('/all', fetchAllUsers);
 router.get('/user', getSpecificUser);
+router.patch('/user/:id', updateUser);
 router.delete('/user/:id', deleteUser);
 
 export default router;
